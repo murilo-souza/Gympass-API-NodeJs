5c100a0b759e2eca780ead49fe756346c8e49507
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-01-09 17:02:54 -0300

    Validate de distanc between user and gym

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -19,8 +19,8 @@ describe('Check-in Use Case', () => {
       title: 'Academia',
       description: '',
       phone: '',
-      latitude: new Decimal(0),
-      longitude: new Decimal(0),
+      latitude: new Decimal(-23.4939464),
+      longitude: new Decimal(-47.458847),
     })
 
     vi.useFakeTimers()
@@ -81,5 +81,25 @@ describe('Check-in Use Case', () => {
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.items.push({
+      id: 'gym-02',
+      title: 'Academia',
+      description: '',
+      phone: '',
+      latitude: new Decimal(-23.3342543),
+      longitude: new Decimal(-47.3524732),
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-02',
+        userId: 'user-01',
+        userLatitude: -23.4939464,
+        userLongitude: -47.458847,
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
